da7650b787968581daf313da9ef76b2d3bd35ece
Full-Hat <68519677+Full-Hat@users.noreply.github.com>
2021-05-27 12:35:30 +0300

    Update utils.h

--- utils.h
+++ utils.h
@@ -21,20 +21,6 @@ public:
         }
         return true;
     }
-
-    static unsigned long long generateUniqueNumber(std::vector<unsigned long long> numbers)
-    {
-        if(numbers.size() == 0)
-            return rand() % 1000000000;
-
-        unsigned long long number = numbers[0];
-        while(std::find(numbers.begin(), numbers.end(), number) != numbers.end())
-        {
-            number = rand() % 1000000000;
-        }
-
-        return number;
-    }
 };
 
 #endif // UTILS_H
